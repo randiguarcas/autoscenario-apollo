aa7d99817013cb095626a761037fe1d4bc27ddda
Randi Guarcas <rguarcas18@gmail.com>
2021-12-29 17:50:40 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { ApolloServer, gql } from "apollo-server";
+import dotenv from "dotenv-safe";
+import schema from "@src/graphql/schema/schema";
+import { performAstCodegen } from "@src/codegen";
+
+dotenv.config();
+
+const server = new ApolloServer({
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+  schema,
+  introspection: true
+});
+
+performAstCodegen();
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Apollo server ready at ${url}`);
+});
